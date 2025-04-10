0c075e59b6ff3d478492553932c2dedbea92d11c
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-04-10 18:26:30 -0300

    Documenting discontinuation due to outdated course version and compatibility issues

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,34 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-    
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionar serviços ao contêiner.
+builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Chama o método Seed
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var seedingService = services.GetRequiredService<SeedingService>();
+    seedingService.Seed();
+}
+
+// Configurar o pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapDefaultControllerRoute();
 
 app.Run();
